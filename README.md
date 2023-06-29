183df8f4cac5e0f686f83675e27b7e2027bb62c4
prachi sarvankar <prachisarvankar.neosoft@gmail.com>
2023-06-29 10:13:59 +0530

    changes in readme file and check console log

--- README.md
+++ README.md
@@ -18,3 +18,31 @@ Project name: app-ssr-vite
 - cd app-ssr-vite
 - npm install
 - npm run dev
+
+next build
+next export
+
+# at this point, we have an out directory as follows
+
+# out/[all html files]
+
+# out/\_next/[all next files]
+
+# and we have a static directory as follows:
+
+# static/$ASSET_PATH/$ASSET_NAME.$ASSET_EXT/$ASSET_HASH.$ASSET_EXT
+
+# Please talk with me once, do let me know ki hum without her kab baat krenge Isipe reply krke code push kr
+
+# the goal is to be able to have this new directory structure reflect what
+
+# we want in S3, so that we can do aws s3 cp ./out s3://$S3_BUCKET/$NAMESPACE/
+
+# (even though, as you'll see, we don't exactly do that).
+
+mv out/\_next . # out/ contains only html files
+mv out \_out
+mkdir -p out/builds/$GITHASH
+mv _out/* out/builds/$GITHASH
+rm -rf \_out
+mv \_next out/
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  console.log(
+    "Please talk with me once, do let me know ki hum without her kab baat krenge. Isipe reply krke code push kr"
+  );
   return (
     <div className="App">
       <div>
@@ -17,18 +19,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
